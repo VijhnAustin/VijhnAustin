37f41e1d636539df44f170c4baf0b86892ccc1a8
VijhnAustin <vijhn.austin@gmail.com>
2022-04-25 08:36:42 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `V.A. Online Portfolio`,
+    description: `A gatsby designed site with a list of my previous projects and work experience.`,
+    author: `@VijhnAustin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
