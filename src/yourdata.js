df4f17c07aec9ffd7980945978bbddb789a7e980
VijhnAustin <vijhn.austin@gmail.com>
2022-07-28 21:05:01 -0500

    Update yourdata.js
    Adding my CSS website to the project section. PROGRESS!!!

--- src/yourdata.js
+++ src/yourdata.js
@@ -39,13 +39,13 @@ export default {
   // Work Section ------------------------
   projects: [
     {
-      title: "Project One", //Project Title - Add Your Project Title Here
-      para:"A collection of my projects across the years. At the moment, they are most centrally located on Github, where these works are open source.",
+      title: "Website designed in CSS", //Project Title - Add Your Project Title Here
+      para:"Designed to display the power of simple CSS and HTML, this website has an impressive landing page with all the necessary info for a small business. ",
       //Project Image - Add Your Project Image Here
       imageSrc:
         "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
-      url: "https://github.com/VijhnAustin",
+      url: "https://leelasbistro.w3spaces.com/",
     },
     {
       title: "Project Two", //Project Title - Add Your Project Title Here
